35956f7937156ec9e1171e6f9ed73741f0a8b05e
Sidik Prasetyo <sidikprasetyo6661@gmail.com>
2025-11-24 12:10:04 +0700

    fixed error queue at portofolio section in home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -36,6 +36,7 @@ interface Portfolio {
   description: string;
   link: string;
   portfolio_images: { id: string; image_url: string }[];
+  created_at: string;
 }
 
 export default function HomePage() {
@@ -399,8 +400,8 @@ export default function HomePage() {
           {/* Cards Grid */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10" data-aos="fade-in">
             {portfolios
-              ?.slice(-3) // ambil 3 item terakhir
-              .reverse() // urutkan agar yang terbaru di atas
+              ?.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime()) // urutkan terbaru dulu
+              .slice(0, 3) // ambil 3 item terakhir
               .map((portfolio) => (
                 <Link key={portfolio.id} href={`/portfolio/${portfolio.id}`} className="group relative transition-shadow duration-300" data-aos="flip-left">
                   <div className="relative overflow-hidden rounded-xl shadow-md hover:shadow-xl transition-all duration-500">
